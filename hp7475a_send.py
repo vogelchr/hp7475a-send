6257f7425230c726ac8155e2b02a417ea731cc0e
Christian Vogel <vogelchr@vogel.cx>
2018-11-24 20:43:45 +0100

    Add better error handling, change buffer handling.
    Initially I was waiting for the buffer to become empty, and
    then feed one complete buffer full of data to the plotter.
    This made the plotting pause momentarily while we detect
    the empty buffer, and start filling again.
    
    Now I'm querying for the actual amount of free space in the
    plotter. But now it turned out that if I just send a few bytes
    to top up the buffer, this may actually be too much, combined
    with my next query for new data.
    
    So, therefore I wait until the buffer (which is 1024 bytes on
    my plotter) reaches 256 free bytes, and then I'll push in the
    number of free bytes minus 128, so that I don't overflow it.
    
    This worked so far, no further buffer overruns happened.

--- hp7475a_send.py
+++ hp7475a_send.py
@@ -1,26 +1,96 @@
 #!/usr/bin/python
-import serial
+import sys
 import time
 
+import serial
+
 # answer to <ESC>.O Output Extended Status Information [Manual: 10-42]
-EXT_STATUS_BUF_EMPTY = 0x08 # buffer empty
+EXT_STATUS_BUF_EMPTY = 0x08  # buffer empty
 EXT_STATUS_VIEW = 0x10  # "view" button has been pressed, plotting suspended
-EXT_STATUS_LEVER = 0x20 # paper lever raised, potting suspended
+EXT_STATUS_LEVER = 0x20  # paper lever raised, potting suspended
 
-def inquire(tty, what) :
-    tty.write(what)
+ERRORS = {
+    # our own code
+    -1: 'Timeout',
+    -2: 'Parse error of decimal return from plotter',
+    # from the manual
+    0: 'no error',
+    10: 'overlapping output instructions',
+    11: 'invalid byte after <ESC>.',
+    12: 'invalid byte while parsing device control instruction',
+    13: 'parameter out of range',
+    14: 'too many parameters received',
+    15: 'framing error, parity error or overrun',
+    16: 'input buffer has overflowed'
+}
 
+
+class HPGLError(Exception):
+    def __init__(self, n, cause=None):
+        self.errcode = n
+        if cause:
+            self.causes = [cause]
+        else:
+            self.causes = []
+
+    def add_cause(self, cause):
+        self.causes.append(cause)
+
+    def __repr__(self):
+        if type(self.errcode) is str:
+            errstr = self.errcode
+        else:
+            errstr = f'Error {self.errcode}: {ERRORS.get(self.errcode)}'
+
+        if self.causes:
+            cstr = ', '.join(self.causes)
+            return f'HPGLError: {errstr}, caused by {cstr}'
+        return f'HPGLError: {errstr}'
+
+    def __str__(self):
+        return repr(self)
+
+
+# read decimal number, followed by carriage return from plotter
+def read_answer(tty):
     buf = bytearray()
-
-    tty.write(b'\033.0')
-    while True :
+    while True:
         c = tty.read(1)
-        if not c :  # timeout
-            return None
-        if c == b'\r' :
+        if not c:  # timeout
+            raise HPGLError(-1)  # timeout
+        if c == b'\r':
             break
         buf += c
-    return int(buf)
+    try:
+        return int(buf)
+    except ValueError as e:
+        print(repr(e))
+        raise HPGLError(-2)
+
+
+def chk_error(tty):
+    tty.write(b'\033.E')
+    ret = None
+    try:
+        ret = read_answer(tty)
+    except HPGLError as e:
+        e.add_cause('ESC.E (Output extended error code).')
+        raise e
+    if ret:
+        raise HPGLError(ret)
+
+def plotter_cmd(tty, cmd, get_answer=False):
+    tty.write(cmd)
+    try:
+        if get_answer:
+            answ = read_answer(tty)
+        chk_error(tty)
+        if get_answer:
+            return answ
+    except HPGLError as e:
+        e.add_cause(f'after sending {repr(cmd)[1:]}')
+        raise e
+
 
 def main():
     import argparse
@@ -47,42 +117,46 @@ def main():
     #     0x04 : monitor mode 1 if set, mode 0 if unset (for terminal)
     #     0x08 : 0: disable monitor mode, 1: enable monitor mode
     #     0x10 : 0: normal mode, 1: block mode
-    tty.write(b'\033.@;0:')  # Plotter Configuration [Manual 10-27]
-    tty.write(b'\033.Y')  # Plotter On [Manual 10-26]
-    tty.write(b'\033.K') # abort graphics
+    try:
+        plotter_cmd(tty, b'\033.@;0:')  # Plotter Configuration [Manual 10-27]
+        plotter_cmd(tty, b'\033.Y')  # Plotter On [Manual 10-26]
+        plotter_cmd(tty, b'\033.K')  # abort graphics
+        plotter_cmd(tty, b'IN;')  # HPGL initialize
+#        plotter_cmd(tty, b'\033.0')  # raise error
+        bufsz = plotter_cmd(tty, b'\033.L', True)  # Output Buffer Size [Manual 10-36]
+    except HPGLError as e:
+        print('*** Error initializing the plotter!')
+        print(e)
+        sys.exit(1)
 
-    bufsz = inquire(tty, b'\033.L')
     print('Buffer size of plotter is', bufsz, 'bytes.')
 
-    is_paused = False
-    while True :
-        status = inquire(tty, b'\033.O')
-        if status is None :
-            print('*** Error: timeout waiting for printer status (command ESC.O).')
-            break
+    total_bytes_written = 0
 
-        if (status & (EXT_STATUS_VIEW | EXT_STATUS_LEVER)) :
-            if not is_paused :
-                print('*** Printer is viewing plot, pausing data.')
-            is_paused = True
+    while True:
+        status = plotter_cmd(tty, b'\033.O', True)
+        if (status & (EXT_STATUS_VIEW | EXT_STATUS_LEVER)):
+            print('*** Printer is viewing plot, pausing data.')
             time.sleep(5.0)
             continue
-        else :
-            if is_paused :
-                print('*** Resuming data.')
-            is_paused = False
 
-        if not (status & EXT_STATUS_BUF_EMPTY) :
-            print('*** Buffer not empty, waiting...')
-            time.sleep(0.5)
+        bufsz = plotter_cmd(tty, b'\033.B', True)
+        if bufsz < 256 :
+            print(f'Only {bufsz} bytes free. :-(', end='\r')
+            sys.stdout.flush()
+            time.sleep(0.25)
             continue
 
-        data = hpgl.read(bufsz)
-        if len(data) == 0 :
+        data = hpgl.read(bufsz - 128)
+        bufsz_read = len(data)
+
+        if bufsz_read == 0 :
             print('*** EOF reached, exiting.')
             break
-        print('*** Writing', len(data), 'bytes to plotter.')
+
+        print(f'{total_bytes_written} byte written. Adding {bufsz_read} ({bufsz} free).')
         tty.write(data)
+        total_bytes_written += bufsz_read
 
 if __name__ == '__main__':
     main()
